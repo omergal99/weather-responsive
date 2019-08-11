5fd6f7b133e8dd2c0950bd9053e4621399e8c05f
omergal99 <omergal99gm@gmail.com>
2019-08-11 04:13:56 +0300

    Updates

---
+++ precache-manifest.434148a673d7feb101bc9abbf2b5e53a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbafb175c363b493d58f5c037ef04be6",
+    "url": "/weather-responsive/index.html"
+  },
+  {
+    "revision": "cadf172a291297360401",
+    "url": "/weather-responsive/static/css/main.597189d0.chunk.css"
+  },
+  {
+    "revision": "f9ccf7135d9ac4dfd330",
+    "url": "/weather-responsive/static/js/2.eddb0dc4.chunk.js"
+  },
+  {
+    "revision": "cadf172a291297360401",
+    "url": "/weather-responsive/static/js/main.dc3a3f74.chunk.js"
+  },
+  {
+    "revision": "2c9ffed4015adffe1ca1",
+    "url": "/weather-responsive/static/js/runtime~main.8f17596a.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/weather-responsive/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "5a3c0694c9b81daf24aff1023a114edb",
+    "url": "/weather-responsive/static/media/SourceSansPro-ExtraLight.5a3c0694.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/weather-responsive/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "09ce18ba63e2ac5919c871d6b8ff6ad0",
+    "url": "/weather-responsive/static/media/background-mobile.09ce18ba.png"
+  },
+  {
+    "revision": "230612ef4021a917d2ac8b267f8a7241",
+    "url": "/weather-responsive/static/media/background.230612ef.png"
+  }
+]);
